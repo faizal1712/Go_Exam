9c0cbbdffe651d39dffd9a2109d3485308bc922c
Faizal17 <54974938+Faizal17@users.noreply.github.com>
2020-12-29 20:38:26 +0530

    Updated jump_game

--- jump_game.go
+++ jump_game.go
@@ -6,12 +6,11 @@ import (
 )
 
 func main() {
-	arr := []int{0, 3, 2, 1, 0, 1, 1, 1, 1}
+	arr := []int{3, 2, 3, 0, 1, 1, 1}
 	fmt.Println("Given array is:", arr)
-	// dest := len(arr)
 	jum := jump(arr)
 	if jum != math.MaxInt64 && jum != math.MinInt64 {
-		fmt.Println("Minimum jump required to reach destination is : ", jum, math.MaxInt64)
+		fmt.Println("Minimum jump required to reach destination is : ", jum)
 	} else {
 		fmt.Println("It's not possible to reach till end !!")
 	}
@@ -23,14 +22,12 @@ func jump(nums []int) int {
 	for i := 1; i < len(nums); i++ {
 		ans[i] = math.MaxInt64
 	}
-	// fmt.Println(ans)
-	// if nums[0] == 0 {
-	// 	return ans[len(ans)-1]
-	// }
+	if nums[0] == 0 {
+		return ans[len(ans)-1]
+	}
 	for i := 0; i < len(nums); i++ {
 		for j := i + 1; (j <= i+nums[i]) && (j < len(nums)); j++ {
 			ans[j] = int(math.Min(float64(ans[j]), float64(ans[i]+1)))
-			// fmt.Println(float64(ans[j]), float64(ans[i]+1))
 		}
 	}
 	return ans[len(ans)-1]
